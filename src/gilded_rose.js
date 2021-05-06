b5b5ffd1791e5a22bca9da5ec3029adbf7988f7c
Thiago Ururay <tufa.araujo@gmail.com>
2021-05-06 12:55:33 -0300

    makes aged brie tests passes

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -28,7 +28,11 @@ class Shop {
   }
 
   _agedBrieUpdate(item) {
-    return;
+    item.sellIn = item.sellIn - 1;
+    if (item.quality >= 50) return;
+
+    item.quality = item.quality + 1;
+    if (item.sellIn <= 0) item.quality = item.quality + 1;
   }
 
   _update(item) {
