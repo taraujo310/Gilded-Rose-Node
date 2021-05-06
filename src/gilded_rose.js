61d169e1cd93ed93caa1495a51dcb19e4edc62e5
Thiago Ururay <tufa.araujo@gmail.com>
2021-05-06 15:18:42 -0300

    makes backstage tests passes

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -40,7 +40,17 @@ class Shop {
   }
 
   _backstageUpdate(item) {
-    return;
+    item.sellIn = item.sellIn - 1;
+
+    if (item.sellIn < 0) {
+      item.quality = 0;
+      return;
+    }
+    if (item.quality >= 50) return;
+
+    item.quality = item.quality + 1;
+    if (item.sellIn < 10) item.quality = item.quality + 1;
+    if (item.sellIn < 5) item.quality = item.quality + 1;
   }
 
   _update(item) {
