dfc1cefd727b5ca37d77e5fc73e36dd01bf50f2f
Thiago Ururay <tufa.araujo@gmail.com>
2021-05-06 12:46:35 -0300

    makes all normal tests passes

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -20,10 +20,12 @@ class Shop {
   }
 
   _normalUpdate(item) {
-    if (item.sellIn > 0)
-      item.quality = item.quality - 1;
-    else
-      item.quality = item.quality - 2;
+    if (item.quality > 0) {
+      if (item.sellIn > 0)
+        item.quality = item.quality - 1;
+      else
+        item.quality = item.quality - 2;
+    }
 
     item.sellIn = item.sellIn - 1;
   }
