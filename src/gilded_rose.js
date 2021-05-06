cbd9f84db9f058f0e8c6d9bf7698fe96fd118a93
Thiago Ururay <tufa.araujo@gmail.com>
2021-05-06 12:44:48 -0300

    makes second and third test of normal pass

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -20,7 +20,11 @@ class Shop {
   }
 
   _normalUpdate(item) {
-    item.quality = item.quality - 1;
+    if (item.sellIn > 0)
+      item.quality = item.quality - 1;
+    else
+      item.quality = item.quality - 2;
+
     item.sellIn = item.sellIn - 1;
   }
 
